f2a4e472b8bce6ad1b6f7a78b3d546939fe3d68c
merterkoc <mert.erkoc@finkoto.com>
2024-08-23 16:47:47 +0300

    Publish sh added.

---
+++ sh/publish.sh
@@ -0,0 +1,13 @@
+cd ..
+cd  honeywell_rfid_reader
+dart pub publish
+cd ..
+
+cd honeywell_rfid_reader_android
+dart pub publish
+cd ..
+
+cd honeywell_rfid_reader_platform_interface
+dart pub publish
+cd ..
+
